b8a11a0b597963a255f45b19e00e9c830842f7e1
Fábio Brito <fabio.brito@atlanteam.com.br>
2022-09-06 23:42:48 -0300

    Teste de Conexão PostgreSQL

--- atentimentos.py
+++ atentimentos.py
@@ -2,4 +2,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+import psycopg2
+
+
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from atendimentos;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
+
 st.title('Serviço de Atendimentos')
